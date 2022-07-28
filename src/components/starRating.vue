022ccbfa9804e33b6274275f5e9e88f44801c9ac
XrustoforK <oleksandr.Kyryliuk@bureau.kyiv.ua>
2022-07-28 09:10:33 +0300

    create page start

---
+++ src/components/starRating.vue
@@ -0,0 +1,28 @@
+<template>
+	<ul class="star-rating d-flex align-items-center">
+		<li v-for="(item, index) in rating" :key="`star-${index}`">
+			 <div class="h2 mb-0">
+				<b-icon icon="star-fill" variant="warning"></b-icon>
+			</div>
+		</li>
+	</ul>
+</template>
+<script>
+export default {
+	props:{
+		rating: { type: Number, default: 5 }
+	}
+}
+</script>
+<style scoped>
+	.star-rating{
+		list-style: none;
+		width: fit-content;
+		padding: 0;
+		align-items: center;
+		margin-bottom: 0;
+	}
+	.star-rating li{
+		padding: 0 2px;
+	}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,80 @@
+<template>
+	<b-container>
+		<div class="mt-3">
+			<b-card-group deck>
+				<b-card bg-variant="white" text-variant="white">
+					<div class="bg-primary text-light p-2 text-center p-0">
+						<h3 class="m-0">ПРОФІЛЬ ЛІКАРЯ</h3>
+					</div>
+					<div class="text-black d-flex mt-2 mb-4">
+						<h3 
+							class="mb-0 d-flex align-items-center"
+						>
+							Тестова Оксана Василівна
+						</h3>
+						<star-rating
+							class="text-black mx-2"
+							:rating="rating"
+						/>
+						<span class="d-flex align-items-center">({{`Середня оцінка ${rating}`}})</span>
+					</div>
+					<b-row>
+						<b-col cols="3">
+							<div
+								class="avatar"
+								style="background-image: url('https://static3.depositphotos.com/1001992/255/i/450/depositphotos_2552495-stock-photo-successful-doctor-with-stethoscope.jpg');"
+							/>
+						</b-col>
+						<b-col cols="9" class="text-black">
+							<b-row class="information">
+								<div class="left">Місто</div>
+								<div class="right">Львів</div>
+							</b-row>
+							<b-row class="information">
+								<div class="left">Місце роботи</div>
+								<div class="right">Львів, площа Ринокб 10</div>
+							</b-row>
+							<b-row class="information">
+								<div class="left">Заклад</div>
+								<div class="right text-primary">Відділення надання екстреної допомоги черговим лікарем у закладі стаціонарної допомоги імені Пилипіва Юрія</div>
+							</b-row>
+						</b-col>
+					</b-row>
+
+				</b-card>
+			</b-card-group>
+		</div>
+	</b-container>
+</template>
+
+<script>
+
+import starRating from '@/components/starRating.vue'
+
+export default {
+	components:{
+		starRating
+	},
+	data:() => ({
+		rating: 3
+	})
+	
+}
+</script>
+<style lang="less" scoped>
+	.avatar{
+		border: 1px solid #ccc;
+		height: 219px;
+		width:  243px;
+		background-size: cover;
+		border-radius: 5px;
+	}
+	.information{
+		text-align: left;
+		.left, .right{
+			display: flex;
+		}
+		.left{ width: 15%; }
+		.right{	width: 50%; }
+	}
+</style>
