fce6296608c98d47b0b93c939ea9cf58af1d8cde
XrustoforK <oleksandr.Kyryliuk@bureau.kyiv.ua>
2022-07-30 21:49:54 +0300

    update router file

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,10 @@ import Profile from '../views/Profile'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/1/profile' },
+  { path: '/:profileId', redirect: '/1/profile' },
   {
-    path: '/',
+    path: '/:profileId/profile',
     name: 'Profile',
     component: Profile
   },
